0992733fcf4d5cc94e897a9b034f64756e4b6099
ubelhj <ubelhj@uw.edu>
2021-06-09 11:43:53 -0700

    initial commit

---
+++ freeflight/freeflight.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "bakkesmod/plugin/bakkesmodplugin.h"
+#include "bakkesmod/plugin/pluginwindow.h"
+
+#include "version.h"
+constexpr auto plugin_version = stringify(VERSION_MAJOR) "." stringify(VERSION_MINOR) "." stringify(VERSION_PATCH) "." stringify(VERSION_BUILD);
+
+
+class freeflight: public BakkesMod::Plugin::BakkesModPlugin/*, public BakkesMod::Plugin::PluginWindow*/
+{
+
+	//std::shared_ptr<bool> enabled;
+
+	//Boilerplate
+	virtual void onLoad();
+	virtual void onUnload();
+
+	// Inherited via PluginWindow
+	/*
+
+	bool isWindowOpen_ = false;
+	bool isMinimized_ = false;
+	std::string menuTitle_ = "freeflight";
+
+	virtual void Render() override;
+	virtual std::string GetMenuName() override;
+	virtual std::string GetMenuTitle() override;
+	virtual void SetImGuiContext(uintptr_t ctx) override;
+	virtual bool ShouldBlockInput() override;
+	virtual bool IsActiveOverlay() override;
+	virtual void OnOpen() override;
+	virtual void OnClose() override;
+	
+	*/
+};
+
